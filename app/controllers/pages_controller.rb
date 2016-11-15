0074562f39ed67578d06a1d2788db4c18c7a8f00
Eugene <e.domotenko@gmail.com>
2016-11-15 22:33:24 +0000

    implement sort by title

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   def index
-    @tasks_uncompleted = current_user.tasks.all.order("created_at desc").where(completed: false)
-    @tasks_completed = current_user.tasks.all.order("created_at desc").where(completed: true)
+    @tasks_uncompleted = current_user.tasks.all.order(params[:sort] || "created_at desc").where(completed: false)
+    @tasks_completed = current_user.tasks.all.order(params[:sort] || "created_at desc").where(completed: true)
     @task  = current_user.tasks.build
   end
 end

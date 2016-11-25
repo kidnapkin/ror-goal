99439833274d4d738613fac75b4fe5d533bf312b
Eugene <e.domotenko@gmail.com>
2016-11-25 23:37:09 +0000

    Add simple authorization

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,13 @@
 class UsersController < ApplicationController
+  before_action :correct_user
+  
   def show
-    @user = current_user
+  end
+  
+  private
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless @user == current_user
   end
 end
